4ee7ca3c125c7e7b0ef9e9d505c4cb326d018b20
pacoman202 <pacoalias380@gmail.com>
2024-03-14 15:05:42 -0500

    cambio en la forma de tomar la paginacion

--- dataTable.js
+++ dataTable.js
@@ -1,65 +1,96 @@
 class DataTable {
     constructor(id) {
-        this.id = id
-        this.header = $(`${id} thead`).html().split('<tr>').map(data => '<tr>' + data).slice(1)
-        this.body = $(`${id} tbody`).html().split('<tr>').map(data => '<tr>' + data).slice(1)
+        this.id = id;
+        const { content, cantContent } = this.obtenerContenido();
+        this.content = content;
+        this.cantContent = cantContent;
+        this.inicializarEstructura();
 
-        this.inicializarEstructura()
+        if (cantContent < 0) return;
+        this.renderizarTabla();
+    }
+
+    obtenerContenido() {
+        const content = [];
+
+        $(`${this.id} thead th`).each(function (index) {
+            const data = {
+                body: [],
+                head: $(this).text()
+            };
+            $(`${this.id} tbody td:nth-child(${index + 1})`).each(function () {
+                data.body.push($(this).text());
+            });
+            content.push(data)
+        });
+
+        const cantContent = content.length - 1
+        return { content, cantContent };
     }
 
     inicializarEstructura() {
-        $(`${this.id}`).wrap('<div class="containerDataTable">')
-        $(`${this.id}`).addClass('dataTable')
+        $(this.id).wrap('<div class="containerDataTable">');
+        $(this.id).addClass('dataTable');
 
-        $(`.containerDataTable`).append('<div class="buttonsDataTable">')
-        $(`.containerDataTable`).prepend(this.generarSelect())
+        $(`.containerDataTable`).append('<div class="buttonsDataTable">');
+        $(`.containerDataTable`).prepend(this.generarSelect());
+        $(`.containerDataTable`).prepend('<div>Buscar:<input type="text" class="searchDataTable"></div>');
 
+        // Evento de cambio de pagina
         $('html').on('click', '.buttonDataTable', function (event) {
-            this.cambiarPagina($(event.target).attr('pag'))
-        }.bind(this))
+            this.cambiarPagina(parseInt($(event.target).attr('pag')));
+        }.bind(this));
 
-        $('html').on('change', '.selectDataTable', this.paginarDatos)
+        // Evento de input para la busqueda de datos
+        $('html').on('change', '.selectDataTable', this.renderizarTabla);
 
-        this.paginarDatos()
+        // Evento de cambio de pagina
+        $('html').on('input', '.searchDataTable', function (event) {
+            this.buscar($(event.target).val());
+        }.bind(this));
+    }
+
+    renderizarTabla() {
+        this.crearBotones();
+        this.cambiarPagina();
     }
 
     generarSelect() {
-        const nums = [1, 5, 10, 25, 50, 100]
-        let select = '<select class="selectDataTable" value="5">'
-        nums.forEach(data => select += `<option value="${data}">${data}</option>`)
-        return select + "</select>"
-    }
-
-    paginarDatos = () => {
-        const cant = parseInt($('.selectDataTable').val())
-
-        this.datosPaginados = []
-        this.body.forEach((data, index) => {
-            if (index % cant === 0) {
-                this.datosPaginados.push([])
-            }
-            this.datosPaginados[Math.floor(index / cant)].push(data)
-        })
-
-        this.crearBotones()
-        this.cambiarPagina(0)
-    }
-
-    cambiarPagina = (data) => {
-        $(`${this.id} tbody`).html(this.datosPaginados[data])
-        this.crearBotones(data)
+        const nums = [1, 5, 10, 25, 50, 100];
+        let select = '<select class="selectDataTable" value="5">';
+        nums.forEach(data => select += `<option value="${data}">${data}</option>`);
+        return select + "</select>";
     }
 
     crearBotones(pagActual = 0) {
-        let botones = []
-
-        this.datosPaginados.forEach((data, index) => {
-            botones.push(`<button pag="${index}" class="buttonDataTable">${index + 1}</button>`)
+        let botones = [];
+        this.content[0].body.forEach((data, index) => {
+            botones.push(`<button pag="${index}" class="buttonDataTable 
+            ${(pagActual === index) ? 'pagActual' : ''}">${index + 1}</button>`);
         })
-        botones = this.mostrarEnRango(parseInt(pagActual), botones)
 
+        $('.buttonsDataTable').html(this.mostrarEnRango(pagActual, botones).join(' '));
+    }
 
-        $('.buttonsDataTable').html(botones.join(' '))
+    paginarDatos = (pagActual = 0) => {
+        const cant = parseInt($('.selectDataTable').val())
+        const punto = pagActual * cant;
+        const data = this.content.map(data => data.body.slice(punto, punto + cant));
+
+        let res = ""
+        for (let i = 0; i < data[0].length; i++) {
+            res += '<tr>';
+            for (let e = 0; e < data.length; e++) {
+                res += `<td>${data[e][i]}</td>`;
+            }
+            res += '</tr>';
+        }
+        return res;
+    }
+
+    cambiarPagina = (numPag = 0) => {
+        $(`${this.id} tbody`).html(this.paginarDatos(numPag));
+        this.crearBotones(numPag);
     }
 
     mostrarEnRango(pagActual, data) {
@@ -73,18 +104,19 @@ class DataTable {
             fin = Math.min(dataLength, fin + 1);
         }
 
+        // data = data.slice(inicio, fin + 1);
         data = data.filter((boton, index) => index >= inicio && index <= fin);
-
-        data.unshift(this.desplazarIzquierda(pagActual))
-        data.push(this.desplazarDerecha(pagActual, dataLength))
-        return data
+        // console.log(data);
+        data.unshift(this.desplazarIzquierda(pagActual));
+        data.push(this.desplazarDerecha(pagActual, dataLength));
+        return data;
     }
 
     desplazarIzquierda(pagActual) {
-        return `<button pag="0" ${(pagActual === 0) ? 'disabled' : ''} class="buttonDataTable"><<</button>`
+        return `<button pag="0" ${(pagActual === 0) ? 'disabled' : ''} class="buttonDataTable"><<</button>`;
     }
 
     desplazarDerecha(pagActual, dataLength) {
-        return `<button pag="${dataLength}" ${(pagActual === dataLength) ? 'disabled' : ''} class="buttonDataTable">>></button>`
+        return `<button pag="${dataLength}" ${(pagActual === dataLength) ? 'disabled' : ''} class="buttonDataTable">>></button>`;
     }
 }
